aa8fae087922be55373b7d35a7f2fac501fce198
xie392 <ymzdjiang@qq.com>
2024-11-20 15:05:38 +0800

    feat: add react-router-dom v7

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles/globals.css'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true}}>
             <App />
         </BrowserRouter>
     </StrictMode>
